0a9503ac29c676e58e208e981d644f04f056101e
parkervg <parkervg5@gmail.com>
2024-08-27 21:37:13 -0400

    Reworking benchmark script

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 This repo is an implementation of the decoding mechanism described in Section 3.2 of [Grammar Prompting for Domain-Specific Language
 Generation with Large Language Models](https://arxiv.org/pdf/2305.19234) by [@berlino](https://github.com/berlino). I refer to the general algorithm as **speculative grammar backtracking**.
 
-It is a form of (rather lenient) constrained decoding, and can be used to guide even proprietary, black-box LLM APIs according to some context-free grammar. 
+It is a form of (rather lenient) constrained decoding, and can be used to guide even proprietary, black-box LLM APIs according to some context-free grammar.
 
 
 ### Features
@@ -15,7 +15,7 @@ It is a form of (rather lenient) constrained decoding, and can be used to guide 
   - OpenAI, Anthropic etc. - as long as you can provide some `generate(prompt: str) -> str` function!
 - Efficient re-use of KV cache for all CausalLM Transformer models
   - Optimistic, speculative decoding = no need to manually update to support new tokenizers
-- Visualization and logging of grammar corrections 
+- Visualization and logging of grammar corrections
 - Token healing to ensure high probability continuations
 
 ## Examples
@@ -24,7 +24,7 @@ It is a form of (rather lenient) constrained decoding, and can be used to guide 
 When using HuggingFace Transformer models, we get an extra speed boost by leveraging efficient caching and backtracking of the KV cache. When a grammar correction is made, we backtrack to the state of the KV cache aligned to the longest prefix that is valid under our Lark context-free grammar.
 ```python
 from transformers import AutoModelForCausalLM, AutoTokenizer
-import guidance 
+import guidance
 
 import grammar_guide as gg
 
@@ -60,7 +60,7 @@ In the visualization below:
 ```python
 import os
 from openai import OpenAI
-import guidance 
+import guidance
 
 import grammar_guide as gg
 
@@ -98,11 +98,11 @@ res = gg.guide(
 
 As described in the paper, one way many existing libraries achieve this goal is by enforcing some constraint at each decoding timestep. For local models, it is possible to pre-process the logit masks such that this is relatively efficient. However, for closed models (think OpenAI, Anthropic, etc.), this can be 'prohitively expensive', since it would require calling the API at each timestep with the full prompt and valid continuation tokens.
 
-Instead, this library takes an optimistic approach to constrained decoding. Autoregressive language models are only going to get better, and often times the overhead of strict, mask-driven constrained decoding isn't worth it. 
+Instead, this library takes an optimistic approach to constrained decoding. Autoregressive language models are only going to get better, and often times the overhead of strict, mask-driven constrained decoding isn't worth it.
 
-For example, if we want gpt-4o to generate some SQLite query, chances are, it'll generate a valid query without any constraints. 
+For example, if we want gpt-4o to generate some SQLite query, chances are, it'll generate a valid query without any constraints.
 
-If there is a mistake, though, we use our grammar to parse the longest prefix that abides by our grammar definition. 
+If there is a mistake, though, we use our grammar to parse the longest prefix that abides by our grammar definition.
 
 ```python
 prediction = "SELECT * FROM students WHERE name SIMILAR TO 'Dan%';"
@@ -111,7 +111,7 @@ prefix, candidates = obtain_correction_pairs(prediction, parser)
 print(prefix)
 # SELECT * FROM students WHERE name
 print(candidates)
-# ['IN', '>', '=', 'NOT', 'BETWEEN', 'LIKE', ...] 
+# ['IN', '>', '=', 'NOT', 'BETWEEN', 'LIKE', ...]
 ```
 Once we have a list of candidates, we can use our draft model to select a valid continuation. In the above example, our candidates are fairly simple strings. However, our grammar may define regular expression continuations as well (e.g. `(?:(?:[A-Z]|[a-z])|_)(?:(?:(?:[A-Z]|[a-z])|[0-9]|_))*`).
 This is powered by the library [guidance](https://github.com/guidance-ai/guidance).
@@ -128,10 +128,12 @@ main_model.predict(prefix + selected_candidate)
 
 
 ### Benchmarks
-The below benchmarks are done on a single A100. They measure the time it takes the respective methods to generate a JSON with exactly 10 string key-value pairs, using [HuggingFaceTB/SmolLM-360M](https://huggingface.co/HuggingFaceTB/SmolLM-360M) and the below prompt.
+The below benchmarks are done on a single A100, with the command `python -m examples.benchmarks.run`.
+
+They measure the time it takes the respective methods to generate a JSON with exactly 10 string key-value pairs, using [HuggingFaceTB/SmolLM-360M](https://huggingface.co/HuggingFaceTB/SmolLM-360M) and the below prompt.
 > Here is a really long JSON object, with 10 keys, using only string values:\n\n```json\n
 
-For most general usecases when using local Transformers models, I highly recommend the library [transformers-CFG](https://github.com/epfl-dlab/transformers-CFG)! 
+For most general usecases when using local Transformers models, I highly recommend the library [transformers-CFG](https://github.com/epfl-dlab/transformers-CFG)!
 
 | Name                                           |   Time Elapsed |   Generation Time |   Tokens Per Second |
 |:-----------------------------------------------|---------------:|------------------:|--------------------:|
--- examples/benchmarks/run.py
+++ examples/benchmarks/run.py
@@ -1,11 +1,21 @@
 from textwrap import dedent
 import guidance
-import outlines
-from tabulate import tabulate
 from functools import partial
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 import time
+from pathlib import Path
+import subprocess
+import sys
+import pandas as pd
+import importlib.util
+
+GRAMMAR_GUIDE_MAX_NEW_TOKENS = 200
+PARENT_DIR = Path(__file__).parent
+
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 
 def load_model(model_name_or_path: str):
@@ -18,7 +28,10 @@ def load_model(model_name_or_path: str):
     return (model, tokenizer)
 
 
-def transformers_cfg(model, tokenizer, grammar_str, prompt):
+def run_transformers_cfg(model, tokenizer, grammar_str, prompt):
+    _has_transformers_cfg = importlib.util.find_spec("transformers_cfg") is not None
+    if not _has_transformers_cfg:
+        install("transformers_cfg")
     from transformers_cfg.grammar_utils import IncrementalGrammarConstraint
     from transformers_cfg.generation.logits_process import (
         GrammarConstrainedLogitsProcessor,
@@ -39,6 +52,7 @@ def transformers_cfg(model, tokenizer, grammar_str, prompt):
         num_return_sequences=1,
         do_sample=False,
     )
+    print("TRANSFORMERS CFG OUTPUT")
     print(tokenizer.decode(output[0], skip_special_tokens=True))
     elapsed_time_seconds = time.time() - start
     elapsed_gen_time_seconds = time.time() - gen_start
@@ -49,7 +63,7 @@ def transformers_cfg(model, tokenizer, grammar_str, prompt):
     )
 
 
-def speculative_grammar_backtracking(model, tokenizer, grammar_str, prompt):
+def run_grammar_guide(model, tokenizer, grammar_str, prompt):
     import grammar_guide as gg
 
     start = time.time()
@@ -63,8 +77,8 @@ def speculative_grammar_backtracking(model, tokenizer, grammar_str, prompt):
         draft_model=guidance.models.Transformers(model_name_or_path, echo=False),
         stop_at=["```", "}"],
         max_grammar_corrections=20,
-        verbose=True,
-        max_new_tokens=200,
+        verbose=False,
+        max_new_tokens=GRAMMAR_GUIDE_MAX_NEW_TOKENS,
         temperature=0.0,
     )
     print(f"SGB OUTPUT (with {res.num_grammar_corrections} corrections)")
@@ -81,7 +95,7 @@ def speculative_grammar_backtracking(model, tokenizer, grammar_str, prompt):
     )
 
 
-def naive_speculative_grammar_backtracking(model, tokenizer, grammar_str, prompt):
+def run_naive_grammar_guide(model, tokenizer, grammar_str, prompt):
     from transformers import pipeline
     import grammar_guide as gg
 
@@ -92,7 +106,7 @@ def naive_speculative_grammar_backtracking(model, tokenizer, grammar_str, prompt
         "text-generation",
         model=model,
         tokenizer=tokenizer,
-        max_new_tokens=200,
+        max_new_tokens=20,
         return_full_text=True,
     )
     res = gg.guide(
@@ -104,7 +118,7 @@ def naive_speculative_grammar_backtracking(model, tokenizer, grammar_str, prompt
         stop_at=["```", "}"],
         max_grammar_corrections=20,
         verbose=False,
-        max_new_tokens=200,
+        max_new_tokens=GRAMMAR_GUIDE_MAX_NEW_TOKENS,
         temperature=0.0,
     )
     print(f"NAIVE SGB OUTPUT (with {res.num_grammar_corrections} corrections)")
@@ -121,25 +135,39 @@ def naive_speculative_grammar_backtracking(model, tokenizer, grammar_str, prompt
     )
 
 
-def outlines_cfg(model_name_or_path, grammar_str, prompt):
-    _, tokenizer = load_model(model_name_or_path)
-    model = outlines.models.transformers(model_name_or_path)
+def run_syncode(model, tokenizer, grammar_str, prompt):
+    _has_syncode = importlib.util.find_spec("syncode") is not None
+    if not _has_syncode:
+        install("git+https://github.com/uiuc-focal-lab/syncode.git")
+    from syncode import Syncode
+
     start = time.time()
-    generator = outlines.generate.cfg(model, grammar_str)
-    sequence = generator(prompt)
-    print("OUTLINES CFG OUTPUT:")
-    print(sequence)
+    # Syncode only accepts the huggingface str id as input
+    syn_llm = Syncode(
+        model=model.config.name_or_path,
+        grammar=grammar_str,
+        parse_output_only=True,
+        mode="grammar_strict",
+        device="cpu",
+    )
+    gen_start = time.time()
+    output = syn_llm.infer(prompt)[0]
+    print("SYNCODE OUTPUT:")
+    print(output)
     elapsed_time_seconds = time.time() - start
+    elapsed_gen_time_seconds = time.time() - gen_start
     return (
         elapsed_time_seconds,
-        len(tokenizer(sequence, add_special_tokens=False, padding=True)["input_ids"])
+        elapsed_gen_time_seconds,
+        len(tokenizer(output, add_special_tokens=False, padding=True)["input_ids"])
         / elapsed_time_seconds,
     )
 
 
 if __name__ == "__main__":
-    model_name_or_path = "HuggingFaceTB/SmolLM-360M"
+    model_name_or_path = "HuggingFaceTB/SmolLM-135M"
     model, tokenizer = load_model(model_name_or_path)
+
     from transformers import pipeline
 
     pipe = pipeline(
@@ -150,45 +178,49 @@ if __name__ == "__main__":
         return_full_text=True,
     )
     pipe("hello")
-    lark_grammar_str = open("./grammars/json.lark").read()
-    ebnf_grammar_str = open("./grammars/json.ebnf").read()
+    lark_grammar_str = open(PARENT_DIR / "json.lark").read()
+    ebnf_grammar_str = open(PARENT_DIR / "json.ebnf").read()
     prompt = dedent(
         """
     Here is a really long JSON object, with 10 keys, using only string values:\n\n```json\n
     """
     )
     # Run benchmarks
-    tabulate = partial(
-        tabulate, headers="keys", showindex="never", tablefmt="simple_outline"
-    )
     name_to_f = {
-        "SGB": partial(
-            speculative_grammar_backtracking,
+        "Grammar Guide": partial(
+            run_grammar_guide,
             model,
             tokenizer,
             lark_grammar_str,
             prompt,
         ),
-        "Naive SGB": partial(
-            naive_speculative_grammar_backtracking,
+        "Naive Grammar Guide": partial(
+            run_naive_grammar_guide,
             model,
             tokenizer,
             lark_grammar_str,
             prompt,
         ),
         "Transformers CFG": partial(
-            transformers_cfg, model, tokenizer, ebnf_grammar_str, prompt
+            run_transformers_cfg, model, tokenizer, ebnf_grammar_str, prompt
         ),
+        "Syncode": partial(run_syncode, model, tokenizer, lark_grammar_str, prompt),
     }
     output = []
+    num_iters = 5
     for name, f in name_to_f.items():
-        time_elapsed, gen_time_elapsed, tokens_per_second = f()
+        time_elapsed, gen_time_elapsed, tokens_per_second = 0, 0, 0
+        for _ in range(num_iters):
+            _time_elapsed, _gen_time_elapsed, _tokens_per_second = f()
+            time_elapsed += _time_elapsed
+            gen_time_elapsed += _gen_time_elapsed
+            tokens_per_second += _tokens_per_second
         output.append(
             {
                 "Name": name,
-                "Time Elapsed": time_elapsed,
-                "Generation Time": gen_time_elapsed,
-                "Tokens Per Second": tokens_per_second,
+                "Time Elapsed": time_elapsed / num_iters,
+                "Generation Time": gen_time_elapsed / num_iters,
+                "Tokens Per Second": tokens_per_second / num_iters,
             }
         )
-    print(tabulate(output))
+    print(pd.DataFrame(output).to_markdown(index=False))
